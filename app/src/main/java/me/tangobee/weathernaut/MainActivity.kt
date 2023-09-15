2a0b65b34fe25e9b4f53e22fcf8171ac921fd5d0
TangoBeee <zonebattle90@gmail.com>
2023-09-15 21:22:58 +0530

    Added a Splash Screen

--- app/src/main/java/me/tangobee/weathernaut/MainActivity.kt
+++ app/src/main/java/me/tangobee/weathernaut/MainActivity.kt
@@ -2,10 +2,17 @@ package me.tangobee.weathernaut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
