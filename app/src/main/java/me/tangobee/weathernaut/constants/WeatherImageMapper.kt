b3ea64d541e4257a8359955bb26de1857c7b0869
TangoBeee <zonebattle90@gmail.com>
2024-05-12 15:32:10 +0530

    feat: added a new icon for fog weather

--- app/src/main/java/me/tangobee/weathernaut/constants/WeatherImageMapper.kt
+++ app/src/main/java/me/tangobee/weathernaut/constants/WeatherImageMapper.kt
@@ -8,7 +8,7 @@ class WeatherImageMapper {
             return when (weatherCode) {
                 0, 1 -> R.drawable.icon_weather_sun
                 2, 3 -> R.drawable.icon_weather_sun_cloud
-                45, 48 -> R.drawable.icon_weather_cloud //TODO("Create A Fog cloud")
+                45, 48 -> R.drawable.icon_weather_cloud_fog
                 in 51..55, in 56..57 -> R.drawable.icon_weather_rain_cloud
                 in 61..65, in 66..67 -> R.drawable.icon_weather_rain_cloud
                 71, 73, 75, 77 -> R.drawable.icon_weather_snow_cloud
